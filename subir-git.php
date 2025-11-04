234efd5780fcb42f1ee369af3faa3a9680d74108
starlix234 <gerina.leiva@gmail.com>
2025-11-04 11:46:59 -0300

    bd

---
+++ subir-git.php
@@ -0,0 +1,7 @@
+echo "# app-buses" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/starlix234/app-buses.git
+git push -u origin main
