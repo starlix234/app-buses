c1ef6748aad3cad80a99bb076b781ef91db8d1f0
starlix234 <gerina.leiva@gmail.com>
2025-11-26 23:32:50 -0300

    pago2

--- bus.sql
+++ bus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-11-2025 a las 20:39:17
+-- Tiempo de generación: 27-11-2025 a las 03:31:56
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -71,6 +71,40 @@ INSERT INTO `estados` (`id_estado`, `estado`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `recargas`
+--
+
+CREATE TABLE `recargas` (
+  `id_recarga` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_tarjeta` int(11) NOT NULL,
+  `monto` int(11) NOT NULL,
+  `estado` varchar(50) DEFAULT NULL,
+  `fecha` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `recargas`
+--
+
+INSERT INTO `recargas` (`id_recarga`, `id_usuario`, `id_tarjeta`, `monto`, `estado`, `fecha`) VALUES
+(1, 3, 2, 2000, 'pendiente', '2025-11-26 22:21:08'),
+(3, 3, 2, 1234, 'pendiente', '2025-11-26 22:45:44'),
+(4, 3, 2, 1234, 'pendiente', '2025-11-26 22:47:09'),
+(5, 3, 2, 1234, 'pendiente', '2025-11-26 22:49:31'),
+(6, 3, 2, 2000, 'pendiente', '2025-11-26 22:52:09'),
+(7, 3, 2, 2000, 'pendiente', '2025-11-26 22:52:43'),
+(8, 3, 2, 100, 'pendiente', '2025-11-26 23:00:14'),
+(9, 3, 2, 100, 'pendiente', '2025-11-26 23:05:16'),
+(10, 3, 2, 2000, 'pendiente', '2025-11-26 23:09:16'),
+(11, 3, 2, 2000, 'pendiente', '2025-11-26 23:10:25'),
+(12, 3, 2, 2000, 'pendiente', '2025-11-26 23:10:45'),
+(13, 3, 2, 2000, 'pendiente', '2025-11-26 23:15:36'),
+(14, 3, 2, 2000, 'pagado', '2025-11-26 23:21:39');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `roles`
 --
 
@@ -101,6 +135,60 @@ CREATE TABLE `rutas` (
   `duracion` time DEFAULT NULL,
   `distancia_km` decimal(5,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tarjetas_tokenizadas`
+--
+
+CREATE TABLE `tarjetas_tokenizadas` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `token_tarjeta` varchar(255) NOT NULL,
+  `ultimos_4` varchar(4) NOT NULL,
+  `tipo_tarjeta` varchar(50) DEFAULT NULL,
+  `marca` varchar(50) DEFAULT NULL,
+  `creada_en` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tarjetas_tokenizadas`
+--
+
+INSERT INTO `tarjetas_tokenizadas` (`id`, `id_usuario`, `token_tarjeta`, `ultimos_4`, `tipo_tarjeta`, `marca`, `creada_en`) VALUES
+(1, 1, 'tok_eb1168052dae77e0', '6623', 'credito', 'visa', '2025-11-26 20:57:46'),
+(2, 3, 'tok_c2a5997b6bded127', '6623', 'credito', 'visa', '2025-11-26 22:00:54');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `transacciones_webpay`
+--
+
+CREATE TABLE `transacciones_webpay` (
+  `id_transaccion` int(11) NOT NULL,
+  `id_recarga` int(11) NOT NULL,
+  `token_ws` varchar(255) DEFAULT NULL,
+  `buy_order` varchar(255) DEFAULT NULL,
+  `session_id` varchar(255) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL,
+  `response_code` int(11) DEFAULT NULL,
+  `authorization_code` varchar(50) DEFAULT NULL,
+  `payment_type` varchar(50) DEFAULT NULL,
+  `installments` int(11) DEFAULT NULL,
+  `transaction_date` datetime DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  `creado_en` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `transacciones_webpay`
+--
+
+INSERT INTO `transacciones_webpay` (`id_transaccion`, `id_recarga`, `token_ws`, `buy_order`, `session_id`, `amount`, `response_code`, `authorization_code`, `payment_type`, `installments`, `transaction_date`, `status`, `creado_en`) VALUES
+(1, 14, '01ab1bf19e8fda0e423ca7520bed2c65edc83cf08b1a9ffee50c755fc7e45a5d', 'recarga-1764210099', 'usuario-3-1764210099', 2000, 0, '1213', 'VN', 0, '2025-11-27 02:21:39', 'pagado', '2025-11-26 23:24:35'),
+(2, 14, '01ab1bf19e8fda0e423ca7520bed2c65edc83cf08b1a9ffee50c755fc7e45a5d', 'recarga-1764210099', 'usuario-3-1764210099', 2000, 0, '1213', 'VN', 0, '2025-11-27 02:21:39', 'pagado', '2025-11-26 23:25:43');
 
 -- --------------------------------------------------------
 
@@ -172,6 +260,14 @@ ALTER TABLE `estados`
   ADD PRIMARY KEY (`id_estado`);
 
 --
+-- Indices de la tabla `recargas`
+--
+ALTER TABLE `recargas`
+  ADD PRIMARY KEY (`id_recarga`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_tarjeta` (`id_tarjeta`);
+
+--
 -- Indices de la tabla `roles`
 --
 ALTER TABLE `roles`
@@ -182,6 +278,20 @@ ALTER TABLE `roles`
 --
 ALTER TABLE `rutas`
   ADD PRIMARY KEY (`id_ruta`);
+
+--
+-- Indices de la tabla `tarjetas_tokenizadas`
+--
+ALTER TABLE `tarjetas_tokenizadas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `transacciones_webpay`
+--
+ALTER TABLE `transacciones_webpay`
+  ADD PRIMARY KEY (`id_transaccion`),
+  ADD KEY `id_recarga` (`id_recarga`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -222,6 +332,12 @@ ALTER TABLE `estados`
   MODIFY `id_estado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT de la tabla `recargas`
+--
+ALTER TABLE `recargas`
+  MODIFY `id_recarga` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
 -- AUTO_INCREMENT de la tabla `roles`
 --
 ALTER TABLE `roles`
@@ -232,6 +348,18 @@ ALTER TABLE `roles`
 --
 ALTER TABLE `rutas`
   MODIFY `id_ruta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tarjetas_tokenizadas`
+--
+ALTER TABLE `tarjetas_tokenizadas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `transacciones_webpay`
+--
+ALTER TABLE `transacciones_webpay`
+  MODIFY `id_transaccion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
@@ -262,6 +390,25 @@ ALTER TABLE `asignaciones_rutas`
 --
 ALTER TABLE `buses`
   ADD CONSTRAINT `buses_ibfk_1` FOREIGN KEY (`id_estado`) REFERENCES `estados` (`id_estado`);
+
+--
+-- Filtros para la tabla `recargas`
+--
+ALTER TABLE `recargas`
+  ADD CONSTRAINT `recargas_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
+  ADD CONSTRAINT `recargas_ibfk_2` FOREIGN KEY (`id_tarjeta`) REFERENCES `tarjetas_tokenizadas` (`id`);
+
+--
+-- Filtros para la tabla `tarjetas_tokenizadas`
+--
+ALTER TABLE `tarjetas_tokenizadas`
+  ADD CONSTRAINT `tarjetas_tokenizadas_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`);
+
+--
+-- Filtros para la tabla `transacciones_webpay`
+--
+ALTER TABLE `transacciones_webpay`
+  ADD CONSTRAINT `transacciones_webpay_ibfk_1` FOREIGN KEY (`id_recarga`) REFERENCES `recargas` (`id_recarga`);
 
 --
 -- Filtros para la tabla `usuarios`
