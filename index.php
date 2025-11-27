0b9a0c89137cee9724d58fb024a5b008f1565d9b
starlix234 <gerina.leiva@gmail.com>
2025-11-27 19:17:07 -0300

    version billetes

--- index.php
+++ index.php
@@ -3,10 +3,13 @@
 <head>
   <meta charset="UTF-8">
   <title>Login - SmartBus</title>
+  <link rel="stylesheet" href="assets/css/estilo-login.css">
 </head>
 <body>
-  <h2>Iniciar Sesión</h2>
   <form action="lib/sesion.php" method="POST">
+   <img src="assets/img/logo.png" class="ancho">
+       <h2>Iniciar Sesión</h2>
+
     <label>Email:</label><br>
     <input type="email" name="email" required><br><br>
     
